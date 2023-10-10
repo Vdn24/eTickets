db1ac94af7311423507eb5908a8c91d8eac554ac
Admin <Admin@DESKTOP-88HORQ3>
2023-10-10 22:00:25 +0300

    It works well

---
+++ eTickets/Controllers/ProducersController.cs
@@ -0,0 +1,84 @@
+﻿using eTickets.Data;
+using eTickets.Data.Services;
+using eTickets.Data.Static;
+using eTickets.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    [Authorize(Roles = UserRoles.Admin)] // атрибут Authorize только зарегистрированые пользователи будут иметь тоступ атрибут Roles устанавливает кому имено будет доступен доступ
+    public class ProducersController : Controller
+    {
+        private readonly IProducersService _service;
+        public ProducersController(IProducersService service)
+        {
+            _service = service;
+        }
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _service.GetAllAsync();
+            return View(allProducers);
+        }
+        //GET: producers/details/1
+        [AllowAnonymous] //"AllowAnonymous" является атрибутом, что определенный метод или контроллер должен быть доступен без аутентификации.
+        public async Task<IActionResult>Details(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+            return View(producerDetails);
+        }
+        //GET: producers/create 
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Create([Bind("ProfilePictureURL, FullName, Bio")]Producer producer)
+        {
+            if (!ModelState.IsValid) return View(producer);
+            await _service.AddAsync(producer);
+            return RedirectToAction(nameof(Index));
+        }
+        //GET: producers/ edit
+        public  async Task<IActionResult> Edit(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+            return View(producerDetails);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Edit(int id, [Bind("Id,ProfilePictureURL, FullName, Bio")] Producer producer)
+        {
+            if (!ModelState.IsValid) return View(producer);
+            if (id == producer.Id)
+            {
+              await _service.UpdateAsync(id, producer);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producer);
+            
+        }
+        //GET: producers/ delete
+        public async Task<IActionResult> Delete(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+            return View(producerDetails);
+        }
+        [HttpPost,ActionName("Delete")]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ eTickets/Models/Producer.cs
@@ -0,0 +1,26 @@
+﻿using eTickets.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class Producer :IEntityBase
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name ="Profile Picture")]
+        [Required(ErrorMessage ="Profile Picture is required")]
+        public string ProfilePictureURL { get; set; }
+        [Display(Name ="Full Name")]
+        [Required(ErrorMessage = "Full Name is required")]
+        public string FullName { get; set; }
+        [Display(Name ="Biography")]
+        [Required(ErrorMessage = "Biography is required")]
+        public string Bio { get; set; }
+        //Relationship
+        public List<Movie> Movies { get; set; }
+    }
+}
